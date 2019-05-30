c8516e78d45f9cbc64d037ce3bbdd1b738649b13
yogiduzit <yverma2@my.bcit.ca>
2019-05-30 14:55:03 -0700

    Crazy rotation going on LOL

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,12 @@
-//import  anime  from "public/anime/lib/anime.min.js";
+
+
+
 const nameAnim = anime({
-  targets: '#name-heading',
-  translateX: 250,
-  //rotate: '2turn',
-  duration: 10000,
-  loop: false
+  targets: ['.alpha', 'button', 'a'],
+  translateX: 0,
+  duration: 1000,
+  rotate: '1turn',
+  loop: true,
+  easing: 'easeInOutQuad'
 });
 
