0e843ddfd731fcaa802ad2a72cd41b972dc98f15
yogiduzit <yverma2@my.bcit.ca>
2019-05-30 09:57:52 -0700

    Basic name animation working

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
-window.onload = function() {
- anime({
-    target: '#name-heading',
-    translateX: 250,
+//import  anime  from "public/anime/lib/anime.min.js";
+const nameAnim = anime({
+  targets: '#name-heading',
+  translateX: 250,
+  //rotate: '1turn',
+  duration: 800,
+  loop: true
+});
 
-  });
-}
