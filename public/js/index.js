840d8f922b0eaf439c544a7561b669d8460faf6c
yogiduzit <yverma2@my.bcit.ca>
2019-05-30 17:52:57 -0700

    translation and rotation

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,30 @@
 
-
-
-const nameAnim = anime({
-  targets: ['.alpha', 'button', 'a'],
-  translateX: 0,
-  duration: 1000,
-  rotate: '1turn',
-  loop: true,
-  easing: 'easeInOutQuad'
+const nameAnim = anime.timeline({
+  autoplay: true
 });
+const moveNameLeft = {
+  targets: '.alpha',
+  translateX: 250,
+  duration: 1000,
+  easing: 'easeInOutSine',
+  delay: anime.stagger(30),
+  endDelay: 400
+};
+
+const rotateName = {
+  targets: '.alpha',
+  rotate: '1turn',
+  duration: 1000,
+  easing: 'easeInOutSine',
+  delay: anime.stagger(30),
+  endDelay: 400,
+  loop: true,
+  offset: '+=100' 
+};
+nameAnim.add(moveNameLeft)
+.add(rotateName);
+//anime.set('.alpha', {backgroundColor: '#ff2801'})
+
+const letters = $('.alpha');
+
 
