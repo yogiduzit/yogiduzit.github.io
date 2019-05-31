1f3584bf47a275c96d0dc4856712208c9f694b59
yogiduzit <yverma2@my.bcit.ca>
2019-05-31 09:47:29 -0700

    Added document.ready

--- public/js/index.js
+++ public/js/index.js
@@ -1,41 +1,41 @@
+$(document).ready(function() {
+  const nameAnim = anime.timeline({
+    autoplay: true
+  });
+  const moveNameLeft = {
+    targets: '.alpha',
+    translateX: '13vmin',
+    duration: 1000,
+    easing: 'easeInOutSine',
+    delay: anime.stagger(30),
+    endDelay: 400
+  };
 
-const nameAnim = anime.timeline({
-  autoplay: true
+  const rotateName = {
+    targets: '.alpha',
+    rotate: '1turn',
+    duration: 1000,
+    easing: 'easeInOutSine',
+    delay: anime.stagger(30),
+    endDelay: 400,
+    loop: true,
+    offset: '+=100' 
+  };
+
+  const play = {
+    targets: '#play-button',
+    rotate: '2turn',
+    duration: 1000,
+    opacity: {
+      value: 1,
+      duration: 300,
+      easing: 'linear'
+    },
+    offset: '+=100' 
+  }
+  nameAnim.add(moveNameLeft).add(rotateName).add(play);
+  //anime.set('.alpha', {backgroundColor: '#ff2801'})
+
+  const letters = $('.alpha');
+
 });
-const moveNameLeft = {
-  targets: '.alpha',
-  translateX: 250,
-  duration: 1000,
-  easing: 'easeInOutSine',
-  delay: anime.stagger(30),
-  endDelay: 400
-};
-
-const rotateName = {
-  targets: '.alpha',
-  rotate: '1turn',
-  duration: 1000,
-  easing: 'easeInOutSine',
-  delay: anime.stagger(30),
-  endDelay: 400,
-  loop: true,
-  offset: '+=100' 
-};
-
-const play = {
-  targets: '#play-button',
-  rotate: '2turn',
-  duration: 1000,
-  opacity: {
-    value: 1,
-    duration: 300,
-    easing: 'linear'
-  },
-  offset: '+=100' 
-}
-nameAnim.add(moveNameLeft).add(rotateName).add(play);
-//anime.set('.alpha', {backgroundColor: '#ff2801'})
-
-const letters = $('.alpha');
-
-
